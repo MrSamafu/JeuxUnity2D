0c4eca4261d5aef3552842e11f19501c7f921112
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-02-09 01:27:42 +0100

    Creation du premier script
    Création du premier script permettant de changer la couleur du sol en rouge en appuyant sur "t"

---
+++ Assets/ScriptInputRedFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptInputRedFloor : MonoBehaviour
+{
+    public GameObject leSol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            leSol.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
