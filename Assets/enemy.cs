91398294f06c2e6d08f58700b0d4ad88f34f5c61
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-02-19 14:33:43 +0100

    New scene With enemy
    add new scene and a followers enemy(comment in script is comming)

---
+++ Assets/enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject player;
+    public Vector2 respawn = new Vector2(-1,1);
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = respawn;
+    }
+}
