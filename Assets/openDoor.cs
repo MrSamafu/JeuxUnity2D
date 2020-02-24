1056783e644ead277fff37d9968d0f3669767d3f
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-02-24 14:45:42 +0100

    création porte automatique
    la porte ce se téléporte lorsqu'un gameobject approche de la porte.

---
+++ Assets/openDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    public GameObject door;
+    public float speed = 10f;
+    public float doorPlaceY;
+    public float doorPlaceX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorPlaceY = door.transform.position.y;
+        doorPlaceX = door.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        door.transform.position = (new Vector2(doorPlaceX, doorPlaceY + 6)); 
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        door.transform.position = (new Vector2(doorPlaceX, doorPlaceY));  
+    }
+}
